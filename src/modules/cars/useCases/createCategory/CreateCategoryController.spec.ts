c86ab541cab0c088650755c1c0f55bac23c91c15
Thiago Rossato <thiago.rossato.tr@gmail.com>
2021-06-04 17:22:28 -0300

    chore: create categ controloller  integration test

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -41,4 +41,23 @@ describe("Create Category Controller", () => {
 
     expect(response.status).toBe(201);
   });
+
+  it("should not be able to create a new category with name exists", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
+    const { token } = responseToken.body;
+    const response = await await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "Description Supertest",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(400);
+  });
 });
